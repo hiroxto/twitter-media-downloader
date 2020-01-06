589b631395908a87656b5f9e463c35f21107056f
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-01-06 23:58:35 +0900

    Create initialize method in IDParser

--- lib/media_downloader/id_parser.rb
+++ lib/media_downloader/id_parser.rb
@@ -2,5 +2,13 @@
 
 module MediaDownloader
   class IDParser
+
+    # @return [String]
+    attr_reader :raw_data
+
+    # @param [String] raw_data パースするID
+    def initialize(raw_data)
+      @raw_data = raw_data
+    end
   end
 end

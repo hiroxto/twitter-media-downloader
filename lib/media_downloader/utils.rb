c9b67c1be71be0eeee151f625e8aedd7a421139e
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-01-07 23:51:13 +0900

    Create Utils.all_media_transform_to_media_wrapper method

--- lib/media_downloader/utils.rb
+++ lib/media_downloader/utils.rb
@@ -3,6 +3,14 @@
 module MediaDownloader
   module Utils
 
+    # ツイートの全てのメディアを MediaWrapper へ変更する
+    #
+    # @param [Twitter::Tweet] tweet
+    # @return [Array<MediaDownloader::MediaWrapper>]
+    def all_media_transform_to_media_wrapper(tweet)
+      tweet.media.map.with_index { |_media, index| create_media_wrapper(tweet, index) }
+    end
+
     # MediaWrapper のインスタンスを作成する
     #
     # @param [Twitter::Tweet] tweet

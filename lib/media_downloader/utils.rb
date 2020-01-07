3e34e093b41bc659b9921a5ed529ce0819cfdcff
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-01-07 23:49:38 +0900

    Create Utils.create_media_wrapper method

--- lib/media_downloader/utils.rb
+++ lib/media_downloader/utils.rb
@@ -3,5 +3,15 @@
 module MediaDownloader
   module Utils
 
+    # MediaWrapper のインスタンスを作成する
+    #
+    # @param [Twitter::Tweet] tweet
+    # @param [Integer] index
+    # @return [MediaDownloader::MediaWrapper]
+    def create_media_wrapper(tweet, index)
+      medias = tweet.media
+      MediaWrapper.new(tweet, medias[index], index)
+    end
+
   end
 end

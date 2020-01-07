10f7662e857c3eb4d920008561ad6a88fce54146
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-01-07 23:38:48 +0900

    Add comments to OptionNumberValidator class

--- lib/media_downloader/option_number_validator.rb
+++ lib/media_downloader/option_number_validator.rb
@@ -13,6 +13,7 @@ module MediaDownloader
       @numbers = numbers
     end
 
+    # バリデーションを行う
     def validate
       empty_array?
       over_medias_size?
@@ -21,14 +22,17 @@ module MediaDownloader
 
     private
 
+    # 空の配列かの確認を行う
     def empty_array?
       raise MediaDownloader::Error::OptionNumberValidatorError, '番号が空です' if @numbers.empty?
     end
 
+    # 番号の最大値が, メディア数を超えていないかの確認を行う
     def over_medias_size?
       raise MediaDownloader::Error::OptionNumberValidatorError, '番号の最大値がメディアの数より多いです' if @numbers.max > @medias.size
     end
 
+    # 全ての番号が存在するかの確認を行う
     def all_exist_number?
       @numbers.each do |number|
         raise MediaDownloader::Error::OptionNumberValidatorError, "#{number}番目のメディアは存在しません" if @medias[number - 1].nil?

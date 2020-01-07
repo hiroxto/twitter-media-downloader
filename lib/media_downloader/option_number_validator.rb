e9481a3961cfdaa16ac47bebdcf25e1643214b6f
Hiroto Kitazawa <hiroto.ktzw@gmail.com>
2020-01-07 22:52:01 +0900

    Implement validator method

--- lib/media_downloader/option_number_validator.rb
+++ lib/media_downloader/option_number_validator.rb
@@ -10,5 +10,21 @@ module MediaDownloader
       @medias = @tweet.media
       @numbers = numbers
     end
+
+    def validate
+      empty_array?
+      over_medias_size?
+    end
+
+    private
+
+    def empty_array?
+      raise '番号が空です' if @numbers.empty?
+    end
+
+    def over_medias_size?
+      raise '番号の最大値がメディアの数より多いです' if @numbers.max >= @medias.size
+    end
+
   end
 end
